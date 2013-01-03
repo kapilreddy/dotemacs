a569cdabeac3feb46c7246258410a4acf067a434
Kapil Reddy <reddy.kapil@gmail.com>
2013-01-03 23:14:05 +0530

    Add android config.

---
+++ configurations/android-config.el
@@ -0,0 +1,46 @@
+(add-path "packages/android-mode/")
+(require 'cedet)
+(require 'android-mode)
+(require 'android)
+
+(setq android-mode-sdk-dir "~/src/android-sdk-macosx/")
+
+(define-key espresso-mode-map "{" 'paredit-open-curly)
+          (define-key espresso-mode-map "}" 'paredit-close-curly-and-newline)
+
+
+(defun android-man (dir)
+  "Look for AndroidManifest.xml file to find project dir of android application."
+  (message dir)
+  (locate-dominating-file dir "AndroidManifest.xml"))
+
+(defmacro android-in-dir (dir body)
+  "Execute BODY form with project dir directory as
+dir directory can be found."
+  `(let ((android-root (android-man ,dir)))
+     (message "Running android in root dir %s" android-root)
+     (when android-root
+       (let ((default-directory android-root))
+         ,body))))
+
+(defun install-run-android ()
+  (interactive)
+  (android-ant-installd)
+  (android-start-app))
+
+(eval-after-load "android-mode"
+ '(progn
+    (define-key android-mode-map
+      (kbd "C-c C-c v")
+      'install-run-android)))
+
+(defun java-setup ()
+  (setq tab-stop-list '(4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64 68 72 76 80 84 88 92)
+    indent-tabs-mode nil
+    tab-width 4
+    fill-column 96
+    c-comment-start-regexp "\\(@\\|/\\(/\\|[*][*]?\\)\\)"))
+
+(add-hook 'android-mode-hook 'java-setup)
+
+(provide 'android-config)
