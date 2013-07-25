0a8c959bc14c78f951583bce21c45b7b599ae124
Kapil Reddy <reddy.kapil@gmail.com>
2013-07-25 17:37:14 +0530

    Basic cljs setup with piggieback keybinding.

--- configurations/cljs-config.el
+++ configurations/cljs-config.el
@@ -2,8 +2,13 @@
 
 (add-to-list 'auto-mode-alist '("\.cljs$" . clojure-mode))
 
-(defun clojurescript-repl ()
- (interactive)
- (run-lisp "/Users/kapil/.bin/lein2 trampoline cljsbuild repl-listen"))
+(require 'nrepl)
+
+(defun piggie-back-cljs ()
+  (interactive)
+  (nrepl-send-string "(do  (require 'cljs.repl.browser) (cemerick.piggieback/cljs-repl :repl-env (doto (cljs.repl.browser/repl-env :port 9000) cljs.repl/-setup)))"
+                     (nrepl-handler (current-buffer))))
+
+(define-key clojure-mode-map (kbd "C-c p a") 'piggie-back-cljs)
 
 (provide 'cljs-config)
