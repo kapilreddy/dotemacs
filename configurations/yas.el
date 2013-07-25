dba27ee34d8ddb04663e7a3153b551e6a7b0514d
Kapil Reddy <reddy.kapil@gmail.com>
2013-07-25 17:44:16 +0530

    Add some helper functions in yas config.

--- configurations/yas.el
+++ configurations/yas.el
@@ -7,4 +7,18 @@
 ;; Load the snippets
 (yas/load-directory yas/root-directory)
 
+(defvar yas/temp-snippet nil
+  "Content of the temporary snippet")
+
+(defun yas/save-temp-snippet ()
+  "Saves the temporary snippet"
+  (interactive)
+  (setq yas/temp-snippet
+        (buffer-substring (region-beginning) (region-end))))
+
+(defun yas/expand-temp-snippet ()
+  "Expands the temporary snippet"
+  (interactive)
+  (yas/expand-snippet yas/temp-snippet))
+
 (provide 'yas)
