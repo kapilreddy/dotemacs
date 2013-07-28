48f39f15b1569fd14cf37358ca5049998d459d4c
Kapil Reddy <reddy.kapil@gmail.com>
2013-07-28 21:03:27 +0530

    Add piggieback config.

--- configurations/cljs-config.el
+++ configurations/cljs-config.el
@@ -4,11 +4,17 @@
 
 (require 'nrepl)
 
-(defun piggie-back-cljs ()
+(defun piggie-back-brepl-cljs ()
   (interactive)
   (nrepl-send-string "(do  (require 'cljs.repl.browser) (cemerick.piggieback/cljs-repl :repl-env (doto (cljs.repl.browser/repl-env :port 9000) cljs.repl/-setup)))"
                      (nrepl-handler (current-buffer))))
 
-(define-key clojure-mode-map (kbd "C-c p a") 'piggie-back-cljs)
+(defun piggie-back-cljs ()
+  (interactive)
+  (nrepl-send-string "(cemerick.piggieback/cljs-repl)"
+                     (nrepl-handler (current-buffer))))
+
+(define-key clojure-mode-map (kbd "C-c p b r") 'piggie-back-brepl-cljs)
+(define-key clojure-mode-map (kbd "C-c p r") 'piggie-back-cljs)
 
 (provide 'cljs-config)
