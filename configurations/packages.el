e1f173161a18978ce2a92ee3e97d6ee82cd818b8
Kapil Reddy <reddy.kapil@gmail.com>
2017-08-30 15:13:18 +0530

    added el-get submodule

--- configurations/packages.el
+++ configurations/packages.el
@@ -1,11 +1,11 @@
 ;; From URL: http://www.emacswiki.org/cgi-bin/wiki/Prelude
 (require 'package)
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(package-initialize)
 
-(package-refresh-contents)
-
+(url-retrieve
+ "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el"
+ (lambda (s)
+   (goto-char (point-max))
+   (eval-print-last-sexp)))
 (defvar required-packages
   '(magit paredit clojure-mode hippie-expand-slime
           yasnippet ac-slime rainbow-delimiters cljsbuild-mode
